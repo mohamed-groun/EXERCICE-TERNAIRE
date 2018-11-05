fbd6c2d72858aab5e8c079eaffb4ee6f295dec75
mohamed-groun <44194988+mohamed-groun@users.noreply.github.com>
2018-11-05 16:24:49 +0100

    Add files via upload

---
+++ EXERCICE-TERNAIRE.php
@@ -0,0 +1,11 @@
+<?php
+$misteryNumber=12 ;
+$myNumber=13 ;
+if ($misteryNumber==$myNumber)
+{ echo "Congratulations !";
+}
+elseif ($misteryNumber>$myNumber ) {echo"+++++++++";}
+else  {echo"--------";}
+
+
+?>
